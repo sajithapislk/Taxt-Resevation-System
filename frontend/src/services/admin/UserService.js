4b9fb17e525b35c2bf949ef9fbba98e8f8659eb3
sajithapislk <sajith@apis.lk>
2024-10-06 09:18:46 +0530

    Update UserService.js

--- frontend/src/services/admin/UserService.js
+++ frontend/src/services/admin/UserService.js
@@ -1,22 +1,14 @@
-UserUpdate: async (userData) => {
-  try {
-    const response = await axios.post(`${API_URL}/admin/user/update`, userData);
-    return response.data; // Return the response data (success message)
-  } catch (error) {
-    // Handle the error response
-    if (error.response && error.response.data) {
-      // If the backend returned an error response
-      return { error: error.response.data.message };
-    } else {
-      // If the error is something else (e.g., network issue)
-      return { error: 'An error occurred. Please try again.' };
-    }
-  }
-},
+import axios from 'axios';
+
+// Get the API URL from the environment variable
+const API_URL = import.meta.env.VITE_API_URL;
+
+// Service object to handle API requests related to user
+const UserService = {
   List: async () => {
     try {
       const response = await axios.get(`${API_URL}/admin/user`);
-      return response.data; // Return the response data (e.g., token)
+      return response.data;
     } catch (error) {
       if (error.response && error.response.data) {
         return { error: error.response.data.message };
@@ -25,7 +17,7 @@ UserUpdate: async (userData) => {
       }
     }
   },
-  UserUpdate: async (userData) => {
+  Update: async (userData) => {
     try {
       const response = await axios.post(`${API_URL}/admin/user/update`, userData);
       return response.data; // Return the response data (success message)
@@ -43,7 +35,7 @@ UserUpdate: async (userData) => {
   Delete: async (data) => {
     try {
       const response = await axios.post(`${API_URL}/admin/user/delete`, data);
-      return response.data; // Return the response data (e.g., token)
+      return response.data;
     } catch (error) {
       if (error.response && error.response.data) {
         return { error: error.response.data.message };
